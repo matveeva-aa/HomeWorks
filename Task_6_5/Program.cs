5dd5f697270294c5e067b7ddbb7cc30cdb1231a4
matveeva-aa <matveeva-aa@V-75-2009>
2025-03-25 21:06:24 +0300

    task 6

---
+++ Task_6_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task_6_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер телефона: ");
+            string input = Console.ReadLine();
+
+            string pattern = @"^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$";
+
+            if (Regex.IsMatch(input, pattern))
+                Console.WriteLine("Номер корректный");
+            else
+                Console.WriteLine("Номер некорректный");
+
+            Console.ReadKey();
+        }
+    }
+}
