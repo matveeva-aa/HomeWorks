aa751262da15200a6d04b95d58f5b83cbc24eeda
matveeva-aa <matveeva-aa@V-75-2009>
2025-06-02 00:16:18 +0300

    task10

---
+++ Task_10_1/Building.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_10_1
+{
+    class Building
+    {
+        protected string _address;
+        protected double _area;
+        protected int _yearBuilt;
+
+        public Building(string address, double area, int yearBuilt)
+        {
+            _address = address;
+            _area = area;
+            _yearBuilt = yearBuilt;
+        }
+
+        public virtual double CalculateTax()
+        {
+            return _area * 1000;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Адрес: {_address}");
+            Console.WriteLine($"Площадь: {_area} кв.м.");
+            Console.WriteLine($"Год постройки: {_yearBuilt}");
+            Console.WriteLine($"Возраст здания: {BuildingAge} лет");
+        }
+
+        public int BuildingAge
+        {
+            get { return DateTime.Now.Year - _yearBuilt; }
+        }
+    }
+}
---
+++ Task_10_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_10_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Building building = new Building("ул. Ленина, 1", 500.0, 1990);
+            building.DisplayInfo();
+            Console.WriteLine($"Налог: {building.CalculateTax()} р.");
+            Console.WriteLine();
+
+            MultiBuilding multiBuilding = new MultiBuilding("Лермонтовский пр, 29", 2000.0, 2005, 10, true);
+            multiBuilding.DisplayInfo();
+            Console.WriteLine($"Налог: {multiBuilding.CalculateTax()} р.");
+            Console.WriteLine();
+
+            Building buildingUpcast = multiBuilding;
+            Console.WriteLine("Приведение производного класса к базовому");
+            buildingUpcast.DisplayInfo();
+            Console.WriteLine($"Налог: {buildingUpcast.CalculateTax()} р.");
+            Console.WriteLine();
+
+            if (buildingUpcast is MultiBuilding downcasted)
+            {
+                Console.WriteLine("Обратное приведение с проверкой типа:");
+                Console.WriteLine($"Средняя площадь на этаж: {downcasted.AreaPerFloor} кв.м.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
