8616f7c8c423d734a22a88606cb4be37d69bd37a
matveeva-aa <matveeva-aa@V-75-2009>
2025-06-23 20:35:09 +0300

    Task 13

---
+++ Task_13_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_13_2
+{
+    public delegate void DeviceStateChangedHandler(string deviceType, string newState, DateTime timestamp);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var home = new SmartHomeSystem();
+
+            home.DeviceStateChanged += (device, state, time) =>
+            {
+                Console.WriteLine($"[{time:HH:mm:ss}] {device}: {state}");
+            };
+
+            home.TurnOnLight();
+            home.SetTemperature(23);
+            home.LockDoor();
+            home.TurnOffLight();
+            home.UnlockDoor();
+            home.SetTemperature(19);
+
+            Console.ReadKey();
+        }
+    }
+}
