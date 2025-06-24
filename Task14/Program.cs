99ea1837dc45ee36b5a2f6a6083daff1aea67894
matveeva-aa <matveeva-aa@V-75-2009>
2025-06-24 20:42:40 +0300

    Task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_14_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> team = new List<string>();
+
+            team.Add("Иванов");
+            team.Add("Петров");
+            team.Add("Сидоров");
+            team.Insert(1, "Козлов");
+
+            bool hasPetrov = team.Contains("Петров");
+            Console.WriteLine($"Есть ли игрок 'Петров' в команде? {hasPetrov}");
+
+            team.Remove("Сидоров");
+
+            int kozlovIndex = team.IndexOf("Козлов");
+            Console.WriteLine($"Индекс игрока 'Козлов': {kozlovIndex}");
+
+            team.Sort();
+
+            Console.WriteLine("\nТекущий состав команды:");
+            for (int i = 0; i < team.Count; i++)
+            {
+                Console.WriteLine($"  {i + 1}. {team[i]}");
+            }
+
+            Console.WriteLine($"\nКоманда пуста? {team.Count == 0}");
+
+            team.Clear();
+            Console.WriteLine($"Количество игроков после очистки: {team.Count}");
+
+            Console.ReadKey();
+        }
+    }
+}
